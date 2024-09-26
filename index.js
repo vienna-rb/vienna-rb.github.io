dd9bb1f9b014684fb1b571ee93c82bc92ca3d601
hschne <hans.schnedlitz@gmail.com>
2024-09-26 21:16:13 +0200

    Try fix blurhash stuff

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import { decode } from "https://cdn.jsdelivr.net/npm/blurhash@2.0.5/+esm";
 
-const hash = "L971fwoeWBj]_4bHWVj[XnWXf6az";
-const canvas = document.getElementById("canvas");
-const bgImg = document.getElementById("bg-img");
-const ctx = canvas.getContext("2d");
+document.addEventListener("DOMContentLoaded", () => {
+  const hash = "L971fwoeWBj]_4bHWVj[XnWXf6az";
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  const bgImg = document.getElementById("bg-img");
 
-// Set canvas size to match window size
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  // Set canvas size to match window size
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-const pixels = decode(hash, canvas.width, canvas.height);
+  const pixels = decode(hash, canvas.width, canvas.height);
 
-const imageData = ctx.createImageData(canvas.width, canvas.height);
-imageData.data.set(pixels);
-ctx.putImageData(imageData, 0, 0);
+  const imageData = ctx.createImageData(canvas.width, canvas.height);
+  imageData.data.set(pixels);
+  ctx.putImageData(imageData, 0, 0);
 
-bgImg.onload = () => {
-  bgImg.style.opacity = "1";
-};
+  bgImg.onload = () => {
+    bgImg.style.opacity = "1";
+  };
+});
